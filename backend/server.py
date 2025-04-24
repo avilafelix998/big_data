6188259a07d454afd5af0b0d0733b69fc1eedae8
devfelix <avilafelix998@gmail.com>
2025-04-24 10:41:08 -0300

    avance

--- backend/server.py
+++ backend/server.py
@@ -9,27 +9,27 @@ CORS(app)
 # Ruta para jugadores promedio por mes
 @app.route('/api/gain', methods=['GET'])
 def get_avg_players():
-    df = pd.read_csv('../data/gain.csv')
+    df = pd.read_csv('./data/gain.csv')
     data = df[['Gain','Date','Game_Name']].to_dict(orient='records')
     return jsonify(data)
 
 @app.route('/api/jugadores_anios', methods=['GET'])
 def get_peak_players_anios():
-    df = pd.read_csv('../data/jugadores_anios.csv')
+    df = pd.read_csv('./data/jugadores_anios.csv')
     # Asegurate que las columnas sean: 'date', 'peakPlayers'
     data = df[['AvgPlayers','Date','Game_Name']].to_dict(orient='records')
     return jsonify(data)
 
 @app.route('/api/jugadores_mes', methods=['GET'])
 def get_peak_players_mes():
-    df = pd.read_csv('../data/jugadores_mes.csv')
+    df = pd.read_csv('./data/jugadores_mes.csv')
     # Asegurate que las columnas sean: 'date', 'peakPlayers'
     data = df[['PeakPlayers','Date','Game_Name']].to_dict(orient='records')
     return jsonify(data)
 
 @app.route('/api/top_promedio', methods=['GET'])
 def get_top_promedio():
-    df = pd.read_csv('../data/top_promedio.csv')
+    df = pd.read_csv('./data/top_promedio.csv')
     # Asegurate que las columnas sean: 'date', 'peakPlayers'
     data = df[['AvgPlayers','Game_Name']].to_dict(orient='records')
     return jsonify(data)
